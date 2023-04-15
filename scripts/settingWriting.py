a082b4d8ee474800c9b90cedf3d05ef729bfe470
Tao Yang <taoyang@cs.utah.edu>
2023-04-15 12:15:59 -0600

    add retrain

--- scripts/settingWriting.py
+++ scripts/settingWriting.py
@@ -5,51 +5,51 @@ import time
 scriptPath=os.path.dirname(os.path.abspath(__file__))
 os.chdir(scriptPath+"/..")
 # experimentSettings={
-#     # "Hard":"config.lambda_hard=1",
-#     "HardWithADWithAQ":"config.lambda_hard=1 config.lambda_Doc=1 config.lambda_Query=1",
-#     "HardWithADWithAQWithPsuedo":"config.lambda_hard=1 config.lambda_Doc=1 config.lambda_Query=1 config.lambda_psuedo=1",
-#     "HardWithAQ":"config.lambda_hard=1 config.lambda_Query=1",
-#     "HardWithAQWithPsuedo":"config.lambda_hard=1 config.lambda_Query=1 config.lambda_psuedo=1",
-#     "HardWithAD":"config.lambda_hard=1 config.lambda_Doc=1",
-#     "AQWADWPsuedo":"config.lambda_psuedo=1 config.lambda_Query=1 config.lambda_Doc=1",
-#     "AQWAD":"config.lambda_Doc=1 config.lambda_Query=1",    
-#     "onlyAQ":"config.lambda_Query=1 config.regularizer.FLOPS.lambda_q=0",    
-#     "onlyAD":"config.lambda_Doc=1 config.regularizer.FLOPS.lambda_d=0 ",
-#     "onlyPsuedo":"config.lambda_psuedo=1 config.regularizer.FLOPS.lambda_d=0",
-#     "onlyADWPsuedo":"config.lambda_psuedo=1 config.lambda_Doc=1 config.regularizer.FLOPS.lambda_d=0",
-#     "AQWPsuedo":"config.lambda_psuedo=1 config.lambda_Query=1",
+#     # "Hard":"config.lambda_hard=10"  ,
+#     "HardWithADWithAQ":"config.lambda_hard=10  config.lambda_Doc=10  config.lambda_Query=10"  ,
+#     "HardWithADWithAQWithPsuedo":"config.lambda_hard=10  config.lambda_Doc=10  config.lambda_Query=10  config.lambda_psuedo=10"  ,
+#     "HardWithAQ":"config.lambda_hard=10  config.lambda_Query=10"  ,
+#     "HardWithAQWithPsuedo":"config.lambda_hard=10  config.lambda_Query=10  config.lambda_psuedo=10"  ,
+#     "HardWithAD":"config.lambda_hard=10  config.lambda_Doc=10"  ,
+#     "AQWADWPsuedo":"config.lambda_psuedo=10  config.lambda_Query=10  config.lambda_Doc=10"  ,
+#     "AQWAD":"config.lambda_Doc=10  config.lambda_Query=10"  ,    
+#     "onlyAQ":"config.lambda_Query=10  config.regularizer.FLOPS.lambda_q=0",    
+#     "onlyAD":"config.lambda_Doc=10  config.regularizer.FLOPS.lambda_d=0 ",
+#     "onlyPsuedo":"config.lambda_psuedo=10  config.regularizer.FLOPS.lambda_d=0",
+#     "onlyADWPsuedo":"config.lambda_psuedo=10  config.lambda_Doc=10  config.regularizer.FLOPS.lambda_d=0",
+#     "AQWPsuedo":"config.lambda_psuedo=10  config.lambda_Query=10"  ,
 # }
 experimentSettings={
-    # "Hard":"config.lambda_hard=1",
-    # "HardWithADWithAQ":"config.lambda_hard=1 config.lambda_Doc=1 config.lambda_Query=1",
-    # "HardWithADWithAQWithPsuedo":"config.lambda_hard=1 config.lambda_Doc=1 config.lambda_Query=1 config.lambda_psuedo=1",
-    # "HardWithAQ":"config.lambda_hard=1 config.lambda_Query=1",
-    # "HardWithAQWithPsuedo":"config.lambda_hard=1 config.lambda_Query=1 config.lambda_psuedo=1",
-    # "HardWithAD":"config.lambda_hard=1 config.lambda_Doc=1",
+    # "Hard":"config.lambda_hard=10"  ,
+    "HardWithADWithAQ":"config.lambda_hard=10 config.lambda_Doc=10 config.lambda_Query=10",
+    "HardWithADWithAQWithPsuedo":"config.lambda_hard=10 config.lambda_Doc=10 config.lambda_Query=10 config.lambda_psuedo=10",
+    "HardWithAQ":"config.lambda_hard=10 config.lambda_Query=10",
+    "HardWithAQWithPsuedo":"config.lambda_hard=10 config.lambda_Query=10 config.lambda_psuedo=10",
+    "HardWithAD":"config.lambda_hard=10 config.lambda_Doc=10",
     
-    "AQWADWPsuedo":"config.lambda_psuedo=1 config.lambda_Query=1 config.lambda_Doc=1",
-    "AQWAD":"config.lambda_Doc=1 config.lambda_Query=1",    
-    "onlyAQ":"config.lambda_Query=1",    
-    "onlyAD":"config.lambda_Doc=1",
-    "onlyADWPsuedo":"config.lambda_psuedo=1 config.lambda_Doc=1",
-    "AQWPsuedo":"config.lambda_psuedo=1 config.lambda_Query=1",
-    "onlyPsuedo":"config.lambda_psuedo=1",
-    "InBatch":"config.inBatch=1",
-    # "InBatchWithAQADPsuedo":"config.inBatch=1 config.lambda_psuedo=1 config.lambda_Query=1 config.lambda_Doc=1",
+    "AQWADWPsuedo":"config.lambda_psuedo=10  config.lambda_Query=10  config.lambda_Doc=10"  ,
+    "AQWAD":"config.lambda_Doc=10  config.lambda_Query=10"  ,    
+    "onlyAQ":"config.lambda_Query=10"  ,    
+    "onlyAD":"config.lambda_Doc=10"  ,
+    "onlyADWPsuedo":"config.lambda_psuedo=10  config.lambda_Doc=10"  ,
+    "AQWPsuedo":"config.lambda_psuedo=10  config.lambda_Query=10"  ,
+    # "onlyPsuedo":"config.lambda_psuedo=10",
+    # "InBatch":"config.inBatch=10"  ,
+    # "InBatchWithAQADPsuedo":"config.inBatch=10  config.lambda_psuedo=10  config.lambda_Query=10  config.lambda_Doc=10"  ,
 }
 
-# desc="full44G"
-# scriptTemplate="runNoGpuspecify.sh"
-# experimentsFolder="experiments/Apr13Mix"
+desc="full44G"
+scriptTemplate="runNoGpuspecify.sh"
+experimentsFolder="experiments/Apr15Retrain"
 
 
 # desc="11GNoReg"
 # scriptTemplate="runNoGpuspecify11GNotch.sh"
 # experimentsFolder="experiments/Apr811GNoReg"
 
-desc="toy"
-scriptTemplate="runToy.sh"
-experimentsFolder="experiments/toy1kMixApr13"
+# desc="toy"
+# scriptTemplate="runToy.sh"
+# experimentsFolder="experiments/toy1kMixApr14Cos"
 # scriptTemplate="runToyNanyuan.sh"
 # scriptTemplate="run11GFullNanyuan.sh"
 # desc="NanYuan11G"
